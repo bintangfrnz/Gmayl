b7c3efbe5cbb9dbf923e254080605655432695a3
Bintang Fajarianto <13519138@std.stei.itb.ac.id>
2023-05-08 18:52:51 +0700

    [Android-Home-SendMail] SendMail data layer

--- android-app/app/src/main/java/com/bintangfajarianto/gmayl/data/database/MailDatabase.kt
+++ android-app/app/src/main/java/com/bintangfajarianto/gmayl/data/database/MailDatabase.kt
@@ -2,9 +2,11 @@ package com.bintangfajarianto.gmayl.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.bintangfajarianto.gmayl.data.model.home.InboxMail
 import com.bintangfajarianto.gmayl.data.model.home.SentMail
 
+@TypeConverters(UserConverters::class)
 @Database(
     entities = [InboxMail::class, SentMail::class],
     version = 1
--- android-app/app/src/main/java/com/bintangfajarianto/gmayl/data/datasource/home/HomeStorageDataSource.kt
+++ android-app/app/src/main/java/com/bintangfajarianto/gmayl/data/datasource/home/HomeStorageDataSource.kt
@@ -2,6 +2,7 @@ package com.bintangfajarianto.gmayl.data.datasource.home
 
 import com.bintangfajarianto.gmayl.data.database.MailDatabase
 import com.bintangfajarianto.gmayl.data.model.home.InboxMail
+import com.bintangfajarianto.gmayl.data.model.home.Mail
 import com.bintangfajarianto.gmayl.data.model.home.SentMail
 import com.bintangfajarianto.gmayl.data.repository.home.HomeStorageRepository
 import kotlinx.coroutines.FlowPreview
@@ -18,4 +19,44 @@ internal class HomeStorageDataSource(
 
     override suspend fun getSentMails(): List<SentMail> =
         mailDatabase.sentMailDao.getSentMails().flatMapConcat { it.asFlow() }.toList()
+
+    override suspend fun sendMail(
+        mail: Mail,
+        toEmail: String,
+        publicKey: String,
+        symmetricKey: String,
+    ) {
+        var formattedBody = mail.body
+
+        // Temporary Encryption
+        if (symmetricKey.isNotBlank()) {
+            formattedBody = TEMP_ENCRYPTED_TAG + formattedBody
+        }
+
+        // Temporary Sign
+        if (publicKey.isNotBlank()) {
+            formattedBody = formattedBody + TEMP_DIGITAL_SIGNATURE_OPEN_TAG + publicKey + TEMP_DIGITAL_SIGNATURE_CLOSE_TAG
+        }
+
+        mailDatabase.sentMailDao.addMailToSent(SentMail(mail).copy(body = formattedBody))
+
+        val isReceive = mail.sender.email == toEmail
+        if (isReceive) {
+            mailDatabase.inboxMailDao.addMailToInbox(InboxMail(mail).copy(body = formattedBody))
+        }
+    }
+
+    override suspend fun deleteInboxMail(mail: InboxMail) {
+        mailDatabase.inboxMailDao.deleteMailFromInbox(mail)
+    }
+
+    override suspend fun deleteSentMail(mail: SentMail) {
+        mailDatabase.sentMailDao.deleteMailFromSent(mail)
+    }
+
+    companion object {
+        private const val TEMP_ENCRYPTED_TAG = "[encrypted] "
+        private const val TEMP_DIGITAL_SIGNATURE_OPEN_TAG = "\n\n<ds>\n"
+        private const val TEMP_DIGITAL_SIGNATURE_CLOSE_TAG = "\n</ds>"
+    }
 }
