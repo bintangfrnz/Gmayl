9e5d6654cd10f2697b57c4553b659ed54b72bf27
Bintang Fajarianto <13519138@std.stei.itb.ac.id>
2023-05-09 21:32:32 +0700

    [Android-Home-MailDetail] Refactor Mail interface by adding encrypted field

--- android-app/app/src/main/java/com/bintangfajarianto/gmayl/data/model/home/InboxMail.kt
+++ android-app/app/src/main/java/com/bintangfajarianto/gmayl/data/model/home/InboxMail.kt
@@ -17,6 +17,7 @@ data class InboxMail(
     override val subject: String,
     override val body: String,
     override val sentTime: String,
+    override val encrypted: Boolean,
     @PrimaryKey(autoGenerate = true)
     override val id: Int? = null,
 ) : Mail {
@@ -29,6 +30,7 @@ data class InboxMail(
         subject = mail.subject,
         body = mail.body,
         sentTime = mail.sentTime,
+        encrypted = mail.encrypted,
         id = mail.id
     )
 }
