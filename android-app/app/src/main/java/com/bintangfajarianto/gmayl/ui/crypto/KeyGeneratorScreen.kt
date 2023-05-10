e465c85e6f119709dea0b653109f609b0511e48b
Bintang Fajarianto <13519138@std.stei.itb.ac.id>
2023-05-10 17:16:42 +0700

    [Android-Crypto] Set up navigation to Generate Key page

---
+++ android-app/app/src/main/java/com/bintangfajarianto/gmayl/ui/crypto/KeyGeneratorScreen.kt
@@ -0,0 +1,9 @@
+package com.bintangfajarianto.gmayl.ui.crypto
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun KeyGeneratorRoute(modifier: Modifier = Modifier) {
+
+}
